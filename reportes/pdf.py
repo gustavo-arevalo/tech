7de8fac75dd268e7104a4579a8c762092df2605c
gustavo-arevalo <claudiogustavoarevalo@gmail.com>
2022-10-18 22:58:53 -0300

    arrancando con pdf

---
+++ reportes/pdf.py
@@ -0,0 +1,11 @@
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import mm
+c=canvas.Canvas("archivoPdf.pdf")
+c.setPageSize((80*mm, 200*mm))
+c.setLineWidth(.3)
+c.setFont('Helvetica',9)
+c.drawString(10,40,"Texto PDF 1")
+c.drawString(10,30,"Texto PDF 2")
+c.drawString(10,20,"Texto PDF 3")
+c.drawString(10,10,"Texto PDF 4")
+c.save()
