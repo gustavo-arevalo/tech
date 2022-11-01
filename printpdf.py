020e6fccab00a8004f7e73674a06cb7f9003f534
gustavo-arevalo <claudiogustavoarevalo@gmail.com>
2022-11-01 00:32:12 -0300

    agregue comentarios de que hace que cosa en printpdf.py

--- printpdf.py
+++ printpdf.py
@@ -11,12 +11,13 @@ currentprinter = "EPSON TM-T88V Receipt" #win32print.GetDefaultPrinter()
 
 #win32api.ShellExecute(0, 'open', GSPRINT_PATH, '-ghostscript "'+GHOSTSCRIPT_PATH+'" -printer "'+currentprinter+'" "ticket.pdf"', '.', 0)
 #win32api.ShellExecute(0, 'open', GSPRINT_PATH, '-ghostscript "'+GHOSTSCRIPT_PATH+'" -printer "'+currentprinter+'" "ticket.pdf"', '.', 0)
-#print('-ghostscript "'+GHOSTSCRIPT_PATH+'" -printer "'+currentprinter+'" "ticket.pdf"')
-#win32api.ShellExecute()
 
+
+    # esas dos lineas imprimen en la impresora por defecto
     #currentprinter = win32print.GetDefaultPrinter()
     #win32api.ShellExecute(0, "print", 'PDFfile.pdf', '/d:"%s"' % currentprinter, ".", 0)
 
 #win32api.ShellExecute(0, "print", "ticket.pdf", None, ".", 0) #comano original
 
+#esta linea imprime en la impresora seleccionada
 win32api.ShellExecute(0, "print", "ticket.pdf", '/d:"%s"' % currentprinter, ".", 0)
